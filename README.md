d95260a3b594ee437c5403cb5b94f1425e452bfd
DianQK <xiaoqing@dianqk.org>
2016-08-18 13:21:02 +0800

    Update README

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Expandable
+
 http://7xokf3.com1.z0.glb.clouddn.com/Demo.mov
+
+## 双向绑定的实践
+
+可以参考： http://swift.gg/2015/12/03/expandable-table-view/
+
+## 运行前
+
+```
+$ sh bootstrap.h
+```
+
+其实就是跑了一下 Carthage 和下载 Rswift 到根目录。
+
+如果下载失败可以直接去
+
+https://github.com/mac-cain13/R.swift/releases
+
+下载，然后拖到根目录（然而并没有失败过 =、=
+
+## 相关文章
+
+[局部-cell-刷新](https://medium.com/@DianQK/局部-cell-刷新-ed9cd1a28910#.iywrf37zf)
+
+> 关于本 Demo 的文章晚一些会写。
---
+++ bootstrap.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+if ! which xcodebuild >/dev/null; then
+  echo "xcodebuild is not available. Install it from https://itunes.apple.com/us/app/xcode/id497799835"
+  exit 1
+fi
+
+if ! which carthage >/dev/null; then
+  echo "installing carthage..."
+  brew update
+  brew install carthage
+fi
+
+if which carthage >/dev/null; then
+  echo "installing carthage dependence..."
+  carthage bootstrap --verbose --platform ios --color auto --no-use-binaries
+fi
+
+if which wget >/dev/null; then
+  echo "installing wget..."
+  brew install wget
+fi
+
+wget https://github.com/mac-cain13/R.swift/releases/download/v2.4.0/rswift-2.4.0.zip
+unzip -n rswift-2.4.0.zip -d ./
+rm rswift*.zip
